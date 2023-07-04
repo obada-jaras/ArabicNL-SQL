4e597cef243f2fd06a4a6271f397eaa490446c63
OBADA TAHAYNA <obada.jaras@yahoo.com>
2023-07-04 19:02:47 +0300

    add `create_dummy_sql.py` - initial

---
+++ python/create_dummy_sql.py
@@ -0,0 +1,86 @@
+import json
+import random
+
+
+def create_insert_for_faculty(faculty_data):
+    statements = []
+    for i, faculty in enumerate(faculty_data, start=1):
+        statements.append(
+            f"INSERT INTO faculty(id, name_arabic, name_english, symbol, description, dean_id) "
+            f"VALUES ({i}, '{faculty['faculty-name']}', '{faculty['faculty-symbol']}', '{faculty['faculty-symbol']}', 'Description {i}', {random.randint(1, 100)});"
+        )
+    return statements
+
+
+def create_insert_for_department(department_data):
+    statements = []
+    for i, department in enumerate(department_data, start=1):
+        statements.append(
+            f"INSERT INTO department(id, faculty_id, name_arabic, name_english, symbol, description, head_instructor_id) "
+            f"VALUES ({i}, {random.randint(1, 100)}, '{department['department-name']}', '{department['department-symbol']}', '{department['department-symbol']}', 'Description {i}', {random.randint(1, 100)});"
+        )
+    return statements
+
+
+def create_insert_for_major(major_data):
+    statements = []
+    for i, major in enumerate(major_data, start=1):
+        statements.append(
+            f"INSERT INTO major(id, department_id, name_arabic, name_english, symbol, description, hours) "
+            f"VALUES ({i}, {random.randint(1, 100)}, '{major['major-name']}', '{major['major-symbol']}', '{major['major-symbol']}', 'Description {i}', {random.randint(10, 40)});"
+        )
+    return statements
+
+
+def create_insert_for_course(course_data):
+    statements = []
+    for i, course in enumerate(course_data, start=1):
+        statements.append(
+            f"INSERT INTO course(id, major_id, name_arabic, name_english, symbol, description, credits) "
+            f"VALUES ({i}, {random.randint(1, 100)}, '{course['course-name']['name-ar']}', '{course['course-name']['name_en']}', '{course['course-symbol']}', 'Description {i}', {int(course['credits'])});"
+        )
+    return statements
+
+
+def create_insert_for_instructor(instructor_data):
+    statements = []
+    for i, instructor in enumerate(instructor_data, start=1):
+        statements.append(
+            f"INSERT INTO instructor(id, name_arabic, name_english, phone_number, email, ritaj_id, office_hours, department_id) "
+            f"VALUES ({i}, '{instructor}', 'Instructor {i}', '123-456-7890', 'email{i}@example.com', 'RITAJ{i}', 'Office Hours {i}', {random.randint(1, 100)});"
+        )
+    return statements
+
+
+def create_insert_for_ta(ta_data):
+    statements = []
+    for i, ta in enumerate(ta_data, start=1):
+        statements.append(
+            f"INSERT INTO teaching_assistant(id, name_arabic, name_english, phone_number, email, ritaj_id, department_id) "
+            f"VALUES ({i}, '{ta}', 'TA {i}', '123-456-7890', 'email{i}@example.com', 'RITAJ{i}', {random.randint(1, 100)});"
+        )
+    return statements
+
+
+def write_to_file(statements, filename):
+    with open(filename, 'w', encoding='utf-8') as f:
+        for statement in statements:
+            f.write(statement + "\n")
+
+
+def generate_sql_inserts(json_filename, sql_filename):
+    with open(json_filename, 'r', encoding='utf-8') as f:
+        data = json.load(f)
+
+    all_statements = []
+    all_statements.extend(create_insert_for_faculty(data['faculty']))
+    all_statements.extend(create_insert_for_department(data['department']))
+    all_statements.extend(create_insert_for_major(data['major']))
+    all_statements.extend(create_insert_for_course(data['course']))
+    all_statements.extend(create_insert_for_instructor(data['instructor']))
+    all_statements.extend(create_insert_for_ta(data['assistant']))
+
+    write_to_file(all_statements, sql_filename)
+
+
+generate_sql_inserts('json/dummy.json', 'sql/output.sql')
